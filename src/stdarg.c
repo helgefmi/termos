3636542a78afa5b2335b801b127c389f3e7bf2fb
Helge Milde <helge@monsternett.no>
2009-03-18 11:19:42 +0100

    - Appended the GNU licence message on every sourcefile

--- src/stdarg.c
+++ src/stdarg.c
@@ -1,7 +1,24 @@
+/* 
+ * This file is part of TermOS.
+ *
+ * TermOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TermOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TermOS.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "stdarg.h"
 #include "tty.h"
 
-char *hex_table = "0123456789ABCDEF";
+char hex_table[] = "0123456789ABCDEF";
 
 int vsprintf(char *buf, const char *fmt, va_list args)
 {
